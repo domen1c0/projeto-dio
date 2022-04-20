3048eceabc26bf978f276632da2dfb84eb836ad5
Paolo Domenico <paolodomenico777@gmail.com>
2022-04-20 17:00:41 -0300

    JS Exceptions

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,4 @@
-function getAdmins(map){
+/*function getAdmins(map){
     let admins = [];
     for([key, value] of map){
         if(value === 'Admin'){
@@ -25,5 +25,39 @@ function valoresUnicos(arr){
     return [...mySet];
 }
 
-console.log(valoresUnicos(meuArray));
+console.log(valoresUnicos(meuArray));*/
+
+/*
+function validaArray(arr, num){
+    try{
+        if(!arr && !num) throw new ReferenceError("Envie os parâmetros");
+        if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object");
+        if(typeof num !== 'number') throw new TypeError("Num precisa ser do tipo number");
+        if(arr.length !== num) throw new RangeError("Tamanho inválido!");
+
+        return arr;
+    }
+    catch(e){
+        if(e instanceof ReferenceError){
+            console.log("Este erro é um ReferenceError!");
+            console.log(e.message);
+        }
+        else if (e instanceof TypeError){
+            console.log("Este erro é um TypeError!");
+            console.log(e.message);
+        }
+        else if (e instanceof RangeError){
+            console.log("Este erro é um RangeError");
+            console.log(e.message);
+        }
+        else{
+            console.log("Tipo de erro não esperado:" + e);
+        }
+
+    }
+}
+
+
+console.log(validaArray([1, 2, 3, 4, 5], 5));*///
+
 
