5cd1b7ef8aefe601d8384403fab11cd885f9701c
Paolo Domenico <paolodomenico777@gmail.com>
2022-04-21 22:09:54 -0300

    Map, Filter e Reduce

---
+++ JavaScript/Map,Filter e Reduce/playground.js
@@ -0,0 +1,73 @@
+//Map
+const maca = {
+    value: 2,
+};
+const laranja = {
+    value: 3,
+};
+function mapComThis(arr, thisArg){
+    return arr.map(function(item){
+        return item * this.value;
+    }, thisArg);
+}
+
+const nums = [1, 2];
+console.log('this -> maçã',mapComThis(nums, maca));
+console.log('this -> laranja',mapComThis(nums, laranja));
+
+function mapSemThis(arr){
+    return arr.map(function(item){
+        return item * 2
+    });
+}
+
+const nums2 = [2, 4, 6, 8, 10];
+
+console.log(mapSemThis(nums2));
+//Filter
+
+function filtraPares(arr){
+    return arr.filter(callback)
+}
+
+function callback(item){
+    return item % 2 === 0;
+}
+
+const meuArray = [1,23,55,67,30,2,4];
+
+console.log(filtraPares(meuArray));
+//Reduce
+function somaNumeros(arr){
+    return arr.reduce(function(prev,current) {
+        return prev + current;
+    })
+}
+
+const arr = [1,2];
+
+console.log(somaNumeros(arr));
+
+const list = [
+    {
+        name: 'sabao em po',
+        preco: 30
+    },
+    {
+        name: 'cereal',
+        preco: 12
+    },
+    {
+        name: 'toalha',
+        preco: 30
+    },
+]
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, list){
+    return list.reduce(function(prev, current){
+        return prev - current.preco;
+    },saldoDisponivel)
+}
+
+console.log(calculaSaldo(saldoDisponivel,list));
